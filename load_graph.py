b1c2d443b2573152f9066cd08e96889cc5837e9e
yangmutong <985777876@qq.com>
2017-05-08 13:46:22 +0800

    change load graph

--- load_graph.py
+++ load_graph.py
@@ -13,7 +13,7 @@ def load_graph(filename, type='directed'):
         arr = line.split('\t')
         edges.append((arr[0], arr[1], 1))
 
-    g.add_weighted_edges_from([(arr[0], arr[1], 1)])
+    g.add_weighted_edges_from(edges)
     load_end = time.time()
     print('load time ', (load_end - load_start))
     return g
