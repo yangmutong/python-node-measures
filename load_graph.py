71a586909776df3290feec7ff26e2ea6ada13fdb
yangmutong <985777876@qq.com>
2017-05-08 19:54:22 +0800

    load_graph change

--- load_graph.py
+++ load_graph.py
@@ -8,10 +8,12 @@ def load_graph(filename, type='directed'):
     if type == 'directed':
         g = nx.DiGraph()
     load_start = time.time()
+    edges = []
     for line in input:
         arr = line.split('\t')
-        g.add_weighted_edges_from([(arr[0], arr[1], 1)])
+        edges.append((arr[0], arr[1], 1))
 
+    g.add_weighted_edges_from(edges)
     load_end = time.time()
     print('load time ', (load_end - load_start))
     return g
